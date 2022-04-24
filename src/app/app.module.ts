73635606f78b865c611e6b64c714a46640de54fb
EdgarAraya <44482655+EdgarAraya@users.noreply.github.com>
2022-04-23 21:39:51 -0400

    sdasd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MostrarIntegranteComponent } from './components/mostrar-integrante/mostrar-integrante.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/integrante-services.service.ts
+++ src/app/services/integrante-services.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +9,16 @@ import { Observable } from 'rxjs';
 export class IntegranteServicesService {
 //test
 
-  private port='8000';
-  private host= '127.0.0.1';
-  private url='http://'+this.host+':'+this.port+'/integrantes';
+  
+  private host= environment.host;
+  private api_integrantes="/integrantes";
+  private url=this.host+this.api_integrantes;
   //private url=this.host_server+"/"+this.api_server;
 
   constructor(private http:HttpClient) { }
 
   getIntegrantes():Observable<any>{
+    console.log("api");
     return this.http.get(this.url)
   }
 
